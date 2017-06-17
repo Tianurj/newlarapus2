da64687ad92d5b1cd1a88c344764cce06ca56a9c
mochammadsepta <151610229@smkassalaambandung.sch.id>
2017-06-17 09:39:25 +0700

    Larapus

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+Route::resource('/middle', 'MiddleController@index');
+
+Route::group(['middleware'=>'web'], function(){
+	Route::group(['prefix'=>'admin','middleware'=>['auth','role:admin']],function(){
+	Route::resource('authors','AuthorsController');
+	Route::resource('books','BooksController');
+
+});
+});
+
+
+
